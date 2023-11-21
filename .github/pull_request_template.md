0c6e0cb9784bb1114eda50be4dda07721f590e30
Zach Wilkins <zach.d.wilkins@gmail.com>
2023-11-21 08:31:53 -0500

    end of section 7

---
+++ .github/pull_request_template.md
@@ -0,0 +1,18 @@
+![Title Template](./pull_request_template/title_template.md)
+
+**What Am I Doing?**
+
+1.  // Feature #1
+2.  // Bug #1
+
+**Why am I doing this?**
+
+// State reason here
+
+**Jira**
+
+// Jira link & story here
+
+Tests Passing
+
+// Image here
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,23 +1,67 @@
+const path = require("path");
 const express = require("express");
+const hbs = require("hbs");
 
 const app = express();
 
-app.get("", (req, res) => {
-  res.send("Hello Express");
-});
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
-app.get("/help", (req, res) => {
-  res.send("Help Page");
+// Setup handlebars engine and views location
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "Zach",
+  });
 });
 
 app.get("/about", (req, res) => {
-  res.send("About Page");
+  res.render("about", {
+    title: "About Me",
+    name: "Zach",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    helpText: "This is some helpful text.",
+    title: "Help",
+    name: "Zach",
+  });
 });
 
 app.get("/weather", (req, res) => {
-  res.send("Show Weather");
+  res.send({
+    forecast: "It is snowing",
+    location: "Philadelphia",
+  });
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Zach",
+    errorMessage: "Help article not found.",
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Zach",
+    errorMessage: "Page not found.",
+  });
 });
 
 app.listen(3000, () => {
-  console.log("Running on port 3000");
+  console.log("Server is up on port 3000.");
 });
