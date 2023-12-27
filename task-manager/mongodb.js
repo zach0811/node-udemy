a5a812c556351b1d2a1c48898626ee0b33bc4466
Zach Wilkins <zach.d.wilkins@gmail.com>
2023-12-27 15:34:13 -0500

    progress through section 10

---
+++ task-manager/mongodb.js
@@ -0,0 +1,31 @@
+const { MongoClient, ObjectId } = require("mongodb");
+
+const connectionURL = "mongodb://127.0.0.1:27017";
+const dbName = "task-manager";
+
+const client = new MongoClient(connectionURL);
+
+async function connectToMongoDB() {
+  try {
+    // Use connect method to connect to the server
+    await client.connect();
+    console.log("Connected successfully to server");
+
+    // db -> reference to our "task-manager" database
+    const db = client.db(dbName);
+
+    const deletedUsers = db
+      .collection("users")
+      .deleteMany({ age: 27 })
+      .then((result) => {
+        console.log(result);
+      });
+  } catch (error) {
+    console.log("Error connecting to MongoDB: ", error);
+  } finally {
+    // Close the connection after the operation
+    // await client.close();
+  }
+}
+
+connectToMongoDB();
